557bf4d60f84bff24f296d7f381aec2042bc4f44
Tim O'Brien <timo@t413.com>
2014-09-11 16:53:42 -0700

    Nav updates. Scrolling now updates nav style.

--- site.js
+++ site.js
@@ -1,21 +1,48 @@
 
+
+var scrollToOffset = 120;
+var disableScrollFn = false;
+
 $(document).ready(function (){
 	$('nav li a').on('click', function(event){
 		event.preventDefault();
 		scrollToID( $(this).attr("href"), 300);
 	});
+
+
+    var sections = {}, navs = {};
+    $('.section').each(function(){
+        sections[this.id] = $(this).offset().top;
+    });
+    $('nav a').each(function(){
+        navs[ $(this).attr('href').substring(1) ] = this.parentNode;
+    });
+
+    $(document).scroll(function(){
+        if (disableScrollFn) { return; }
+        var page_height = $(window).height();
+        var pos = $(this).scrollTop();
+        for (i in sections) {
+            if ((pos + scrollToOffset >= sections[i]) && sections[i] < pos + page_height){
+                $("nav li").removeClass('active');
+                $(navs[i]).addClass('active');
+            }
+        }
+    });
+
 });
 
 
+
+
 function scrollToID(id, speed){
-	var offSet = 50;
-	var targetOffset = $(id).offset().top - offSet;
-	var mainNav = $('#main-nav');
-	$('html,body').animate({scrollTop:targetOffset}, speed);
-	if (mainNav.hasClass("open")) {
-		mainNav.css("height", "1px").removeClass("in").addClass("collapse");
-		mainNav.removeClass("open");
-	}
+	var targetOffset = $(id).offset().top - scrollToOffset;
+    disableScrollFn = true;
+    $("nav li").removeClass('active');
+	$('html,body').animate({scrollTop: targetOffset}, speed, "linear", function(){
+        disableScrollFn = false;
+        console.log("animateion completed.");
+    });
 }
 if (typeof console === "undefined") {
     console = {
