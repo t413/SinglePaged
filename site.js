ca65ecfaf86c0b86472d0e81cd00fbda0bad0177
Tim O'Brien <timo@t413.com>
2014-09-11 12:10:12 -0700

    Adding javascript animated scroll-to

---
+++ site.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function (){
+	$('nav li a').on('click', function(event){
+		event.preventDefault();
+		scrollToID( $(this).attr("href"), 300);
+	});
+});
+
+
+function scrollToID(id, speed){
+	var offSet = 50;
+	var targetOffset = $(id).offset().top - offSet;
+	var mainNav = $('#main-nav');
+	$('html,body').animate({scrollTop:targetOffset}, speed);
+	if (mainNav.hasClass("open")) {
+		mainNav.css("height", "1px").removeClass("in").addClass("collapse");
+		mainNav.removeClass("open");
+	}
+}
+if (typeof console === "undefined") {
+    console = {
+        log: function() { }
+    };
+}
