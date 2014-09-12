a40bbbf03f772e809fe40c5c425aba89493b737f
Tim O'Brien <timo@t413.com>
2014-09-11 17:53:28 -0700

    Make scrolling into a jQuery plugin called navScroller

--- site.js
+++ site.js
@@ -1,51 +1,62 @@
 
+(function( $ ) {
 
-var scrollToOffset = 120;
-var disableScrollFn = false;
+    var settings;
+    var disableScrollFn = false;
+    var navs = {}, sections = {};
+
+    $.fn.navScroller = function(options) {
+        settings = $.extend({
+            scrollToOffset: 120,
+            scrollSpeed: 300,
+            activateParentNode: true,
+        }, options );
+
+        //attatch click listeners
+    	this.on('click', function(event){
+    		event.preventDefault();
+            var navID = $(this).attr("href").substring(1);
+            disableScrollFn = true;
+            activateNav(navID);
+        	$('html,body').animate({scrollTop: sections[navID] - settings.scrollToOffset},
+                settings.scrollSpeed, "linear", function(){
+                    disableScrollFn = false;
+                }
+            );
+    	});
+
+        //populate lookup of clicable elements and destination sections
+        this.each(function(){
+            var scrollID = $(this).attr('href').substring(1);
+            navs[scrollID] = (settings.activateParentNode)? this.parentNode : this;
+            sections[scrollID] = $(document.getElementById(scrollID)).offset().top;
+            console.log("#"+scrollID + " nav:"+navs[scrollID] + " section:"+sections[scrollID]);
+        });
+
+        // setup scroll listener
+        $(document).scroll(function(){
+            if (disableScrollFn) { return; }
+            var page_height = $(window).height();
+            var pos = $(this).scrollTop();
+            for (i in sections) {
+                if ((pos + settings.scrollToOffset >= sections[i]) && sections[i] < pos + page_height){
+                    console.log("#"+i + " pos:"+pos + " settings.scrollToOffset:" + settings.scrollToOffset);
+                    activateNav(i);
+                }
+            }
+        });
+    };
+
+    function activateNav(navID) {
+        for (nav in navs) { $(navs[nav]).removeClass('active'); }
+        $(navs[navID]).addClass('active');
+    }
+})( jQuery );
+
 
 $(document).ready(function (){
-	$('nav li a').on('click', function(event){
-		event.preventDefault();
-		scrollToID( $(this).attr("href"), 300);
-	});
 
-
-    var sections = {}, navs = {};
-    $('.section').each(function(){
-        sections[this.id] = $(this).offset().top;
-    });
-    $('nav a').each(function(){
-        navs[ $(this).attr('href').substring(1) ] = this.parentNode;
-    });
-
-    $(document).scroll(function(){
-        if (disableScrollFn) { return; }
-        var page_height = $(window).height();
-        var pos = $(this).scrollTop();
-        for (i in sections) {
-            if ((pos + scrollToOffset >= sections[i]) && sections[i] < pos + page_height){
-                $("nav li").removeClass('active');
-                $(navs[i]).addClass('active');
-            }
-        }
-    });
+    $('nav li a').navScroller();
 
 });
 
-
-
-
-function scrollToID(id, speed){
-	var targetOffset = $(id).offset().top - scrollToOffset;
-    disableScrollFn = true;
-    $("nav li").removeClass('active');
-	$('html,body').animate({scrollTop: targetOffset}, speed, "linear", function(){
-        disableScrollFn = false;
-        console.log("animateion completed.");
-    });
-}
-if (typeof console === "undefined") {
-    console = {
-        log: function() { }
-    };
-}
