e1329b51f893407d08f754762b553242a481ac64
Fernando Danko <fernandodanko@gmail.com>
2021-03-26 04:16:38 -0300

    Fix rose no se cierra

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function clickListener(e){
 	e.preventDefault(); // Cancel the native event
 	
 	alertify.alert("Importante!","<img src='img/rose.jpg'></img>",function(e){e.preventDefault()}).set('closable', false).set('label', '¡Lea!'); ;
-	setTimeout(()=>(window.location.href = e.target.href), 5000);
+	setTimeout(()=>(alertify.closeAll(); window.location.href = e.target.href), 5000);
 	alertify.notify('Procesando...', 'success',10)
 }
 
